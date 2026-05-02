fb26a9a947b706c6f29287c45978120caa8e0488
Kostas.Stergiannis <kostas.stergiannis98@gmail.com>
2026-05-02 15:13:01 +0300

    NailHub Feat: Add splash screen and optimize the code.

---
+++ NailHubTest/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  NailHubTest
+//
+//  Created by Konstantinos Stergiannis on 2/5/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ NailHubTest/WelcomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeScreen.swift
+//  NailHubTest
+//
+//  Created by Konstantinos Stergiannis on 2/5/26.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeScreen()
+}
